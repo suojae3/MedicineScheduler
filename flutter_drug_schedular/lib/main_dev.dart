b5ed54f9150f1b0cdc440427e95fc16448ea8913
suojae3 <suojae3@gmail.com>
2024-05-24 20:11:51 +0900

    ⚙️ config: add dart file to set dev/prod environment (#6)

---
+++ flutter_drug_schedular/lib/main_dev.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_drug_schedular/global.dart';
+import 'package:flutter_drug_schedular/main.dart';
+
+
+
+void main() {
+  Global.baseURL = "https://flutter_drug_schedular/dev/";
+  runApp(const MyApp());
+}
+
---
+++ flutter_drug_schedular/lib/main_prod.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_drug_schedular/global.dart';
+import 'package:flutter_drug_schedular/main.dart';
+
+
+
+void main() {
+  Global.baseURL = "https://flutter_drug_schedular/prod/";
+  runApp(const MyApp());
+}
